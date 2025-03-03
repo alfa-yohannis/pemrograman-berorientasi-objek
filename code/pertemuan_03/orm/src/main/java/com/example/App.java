c5aa67afc77a9a85cbd027695f9a577ebd8fedfa
alfa-ryano <ary506@york.ac.uk>
2025-03-03 11:14:39 +0700

    Add code for chapter 4

--- code/pertemuan_03/orm/src/main/java/com/example/App.java
+++ code/pertemuan_03/orm/src/main/java/com/example/App.java
@@ -1,7 +1,27 @@
 package com.example;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.example.model.User;
+
 public class App {
+
+
     public static void main(String[] args) {
-        
+        Session session = MultiDatabaseFactory.getMysqlSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        User user = new User("MySQL Test User");
+        Long generatedId = (Long) session.save(user);
+
+        transaction.commit();
+        session.close();
+
+        Session newSession = MultiDatabaseFactory.getMysqlSessionFactory().openSession();
+        User savedUser = newSession.get(User.class, generatedId);
+        newSession.close();
+
+        System.out.println("MySQL - Retrieved User ID: " + savedUser.getId());
     }
 }
